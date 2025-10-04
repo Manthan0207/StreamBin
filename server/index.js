43cd8e98d8cdd08cfcafd11ab526f14226bb12b4
Manthan-Panseriya <manthan0205@gmail.com>
2025-10-04 13:20:50 +0530

    Basic Project SetUp and Database connection done

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import { ConnectDB } from './db/connectDB.js';
 
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+    await ConnectDB();
     console.log("Server running on port 3000");
 });
